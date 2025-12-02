3261d7a85caa06efad5a34174a016146a195dec2
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2025-12-02 13:59:59 +0100

    init typo3 ext

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Powermail Salesforce',
+    'description' => 'Add a finisher to send data to salesforce',
+    'category' => 'plugin',
+    'author' => 'Thomas Rawiel',
+    'author_email' => 'thomas.rawiel@gmail.com',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.18-13.4.99',
+            'powermail' => '12.0.0-13.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
